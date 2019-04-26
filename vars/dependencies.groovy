30e4a575f3b2c9228e350541eb7354b27331a007
Becirbegovic Edin (IT-SWE-CC3-TS3) <edin.becirbegovic@sbb.ch>
2019-04-26 19:13:11 +0200

    edit skript agian and again

--- vars/dependencies.groovy
+++ vars/dependencies.groovy
@@ -1,4 +1,6 @@
-def readPOM() {
+def call (String name = 'Edin') {
+    echo "hello, ${name}."
+
     POM = sh ( script: 'cat build.gradle', returnStdout: true)
     def list = readGradleBuild file: 'build.gradle'
 
@@ -8,6 +10,18 @@ def readPOM() {
     return POM;
     echo "Hallo"
 
-
 }
-return this
+
+//def readPOM() {
+  //  POM = sh ( script: 'cat build.gradle', returnStdout: true)
+    //def list = readGradleBuild file: 'build.gradle'
+
+    //assert list instanceof groovy.util.Node
+    //assert list.technology.name.text() == 'Groovy'
+
+    //return POM;
+    //echo "Hallo"
+
+
+//}
+//return this
